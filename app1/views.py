6184d766cc50645d7750f40adda5bae52a90ac66
sanny gupta <sannygupta79@gmail.com>
2024-08-19 10:40:34 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def sanny(requset):
+    return HttpResponse('nice and everything are good')
+
+
+def Gupta(requset):
+    return HttpResponse('okk and nice')
+
+
+def kudra(requset):
+    return HttpResponse('from kudra in Bihar')
+
+
+
+def patna(requset):
+    return HttpResponse('patna in India')
